7684e66ae625255b3ca7bc886ebf91ddd04d4ded
Muhammad Afi Ramadhan <145359669+afiramadhan@users.noreply.github.com>
2023-10-10 21:28:15 +0100

    Add files via upload

---
+++ CR2.py
@@ -0,0 +1,14 @@
+def student_pass(score1, score2, score3):
+    #checking if the student pass all subjects at 40 points and above
+    if score1 >= 40 and score2 >= 40 and score3 >= 40:
+        print("This student has passed.")
+    #checking if the student pass at least two subjects at 40 points or above
+    #and the overall average is strictly greater than 50 points (so 50 points exactly doesn't meet the requirement).
+    elif (score1 >= 40 and score2 >= 40) or (score1 >= 40 and score3 >= 40) or (score2 >= 40 and score3 >= 40): #three possibilities (1,2; 1,3; 2,3) from n(n+1)/2 possibilities
+        average_score = (score1 + score2 + score3) / 3 #defining the average
+        if average_score > 50:
+            print("This student has passed.")
+        else:
+            print("This student has not passed.")
+    else:
+        print("This student has not passed.")
