2e5b31ec06c65b8a618cc29d60afea482459d400
Muhammad Afi Ramadhan <145359669+afiramadhan@users.noreply.github.com>
2023-10-18 19:52:36 +0100

    Add files via upload

---
+++ CR2 (2).py
@@ -0,0 +1,6 @@
+def student_pass(score1, score2, score3):
+    t=sorted([score1,score2,score3]) #make a sorted list of all the scores
+    if t[0]>=40 or (t[1]>=40 and sum(t)/3>50): #if smallest score is still at least 40, or second smallest is at least 40 with an average greater than 50, then the student passes
+        print('This student has passed.')
+    else: #in all other cases, the student fails
+        print('This student has not passed.')
