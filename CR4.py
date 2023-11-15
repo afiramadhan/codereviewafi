de95693cc97ec0293c63ecc90511ef611bf6277c
Muhammad Afi Ramadhan <145359669+afiramadhan@users.noreply.github.com>
2023-11-15 14:26:06 +0000

    paraphrased

--- CR4.py
+++ CR4.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def Road_AfterTstep(R_old, T):
     '''
-    Function that uses update rules to derive the state of the road after T steps. 
+    A function that, after T steps, determines the condition of the road using update rules. 
     
     Args:
-    R_old:   numpy array, the initial state of the road where each element represents a cell. 
-              R_old[i] is 0 if there is no car in the cell (i) and is 1 when there is a car in this cell.
+    R_old:   numpy array, the original state of the road, where a cell is represented by each element. 
+    When a car is present in this cell, R_old[i] is 1; otherwise, it is 0 when it is not.
     T:        Integer, number of time steps to update the road.
     
     Returns:
@@ -15,20 +15,20 @@ def Road_AfterTstep(R_old, T):
     road_length = len(R_old)
     
     for _ in range(T):
-        # Create a copy of the current state to avoid updating in-place
+        #to avoid updating in-place, we firstly make a copy of the current state.
+
         R_new = np.zeros(road_length, dtype=int)
 
         for i in range(road_length):
             next_position = (i + 1) % road_length
-            # Check if the current cell has a car
+            #then we check if the current cell has a car
             if R_old[i] == 1 and R_old[next_position] == 0:
-                # Move the car to the next cell (considering periodic boundary)
-                # next_position = (i + 1) % road_length
+                #we then proceed to the next cell with the car, taking into account the periodic border.
                 R_new[next_position] = 1
             elif R_old[i] == 1 and R_old[next_position] == 1:
                 R_new[i] = 1
 
-        # Update the old state for the next iteration
+        #we update the old state for the next iteration
         R_old = np.copy(R_new)
 
     return R_new
